3f12d3438eccea146688f87199c93de135b42037
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-01-20 00:33:17 +0600

    drawer done

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,39 @@ class HomePage  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title: Text("Practice1 app"),
-
+      appBar: AppBar(
+        title: const Text('Drawer Demo'),
       ),
-      body: Container(
-        child: Center(child: Text("Hello everyone")),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
       ),
     );
   }
